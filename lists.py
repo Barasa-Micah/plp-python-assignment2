20ef80377c7bcb5f8d04d463fa31da8cc692caf7
Mica <132666987+Barasa-Micah@users.noreply.github.com>
2024-08-29 19:04:43 +0300

    Add lists.py

--- lists.py
+++ lists.py
@@ -1,16 +1,16 @@
 my_list = []
 
 # Append elements to the list
-my_list.append(14)
-my_list.append(19)
-my_list.append(24)
-my_list.append(29)
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
 
 
-my_list.insert(1, 16)
+my_list.insert(1, 15)
 
 
-my_list.extend([34, 39, 44])
+my_list.extend([50, 60, 70])
 
 
 my_list.pop()
@@ -19,8 +19,8 @@ my_list.pop()
 my_list.sort()
 
 
-index_of_24 = my_list.index(24)
-print("Index of 24:", index_of_24)
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
 
 
 print("Final list:", my_list)

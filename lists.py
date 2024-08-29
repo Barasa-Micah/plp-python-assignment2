c73b58d55eed2726f7ea08fac1632b7d9875e564
Mica <132666987+Barasa-Micah@users.noreply.github.com>
2024-08-29 18:57:44 +0300

    Add lists.py

---
+++ lists.py
@@ -0,0 +1,26 @@
+my_list = []
+
+# Append elements to the list
+my_list.append(14)
+my_list.append(19)
+my_list.append(24)
+my_list.append(29)
+
+
+my_list.insert(1, 16)
+
+
+my_list.extend([34, 39, 44])
+
+
+my_list.pop()
+
+
+my_list.sort()
+
+
+index_of_24 = my_list.index(24)
+print("Index of 24:", index_of_24)
+
+
+print("Final list:", my_list)
